3f3de989e192ed762222dde59a5d8b25c9d24e59
Paul Rignall <paul.rignall@gmail.com>
2022-06-08 21:58:49 +0100

    Add basic MariaDB info

---
+++ soda/mariadb/soda/data_sources/mariadb_data_source.py
@@ -0,0 +1,31 @@
+import logging
+# from typing import Dict, List, Optional
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+import mariadb
+from soda.common.exceptions import DataSourceConnectionError
+from soda.execution.data_source import DataSource
+from soda.execution.data_type import DataType
+
+logger = logging.getLogger(__name__)
+
+
+class DataSourceImpl(DataSource):
+    TYPE = "mariadb"
+
+    def connect(self, connection_properties):
+        self.connection_properties = connection_properties
+        try:
+            self.connection = mariadb.connect(
+                host=connection_properties.get("host"),
+                user=connection_properties.get("username"),
+                password=connection_properties.get("password"),
+                database=connection_properties.get("database"),
+                port=connection_properties.get("port"),
+                connect_timeout=connection_properties.get("connect_timeout"),
+            )
+            return self.connection
+
+        except Exception as e:
+            raise DataSourceConnectionError(self.TYPE, e)
