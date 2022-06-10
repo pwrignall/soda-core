c7e30756f22b6710c5b86fe7aa99615df7993073
Paul Rignall <paul.rignall@gmail.com>
2022-06-10 21:48:50 +0100

    Update version to rc1

--- soda/mariadb/setup.py
+++ soda/mariadb/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 8):
     sys.exit(1)
 
 package_name = "soda-core-mariadb"
-package_version = "3.0.0b19"
+package_version = "3.0.0rc1"
 description = "Soda Core MariaDB Package"
 
 requires = [
